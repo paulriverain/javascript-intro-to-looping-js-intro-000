bb01a7769cce5f1020b8646fe87f63c79b8730c5
Paul Riverain <paulriverain@yahoo.com>
2018-12-28 22:00:32 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,7 +2,7 @@ function forLoop(){
   const list= [];
     const t = 10
   for(let i = 0; i < t; i++){
-    list.push('I am 1 strange loop.')
+    list.push(`I am ${i} strange loop.`)
   }
   return [list, t]
 }
